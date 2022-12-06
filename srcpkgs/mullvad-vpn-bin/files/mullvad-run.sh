840570336c9681e38982a1a2e6b2bd651669b557
intrnl <intrnl28+git@gmail.com>
2022-12-06 07:27:26 +0700

    mullvad-vpn-bin: make split tunneling work

--- srcpkgs/mullvad-vpn-bin/files/mullvad-run.sh
+++ srcpkgs/mullvad-vpn-bin/files/mullvad-run.sh
@@ -2,5 +2,11 @@
 
 sv check dbus >/dev/null || exit 1
 
+# make mullvad-exclude actually work properly by creating the responsible file
+# ahead of time with the correct permissions
+mkdir -p /sys/fs/cgroup/net_cls/mullvad-exclusions/
+touch /sys/fs/cgroup/net_cls/mullvad-exclusions/cgroup.procs
+chmod 777 /sys/fs/cgroup/net_cls/mullvad-exclusions/cgroup.procs
+
 MULLVAD_RESOURCE_DIR=/opt/Mullvad\ VPN/resources/
 exec /usr/bin/mullvad-daemon -v --disable-stdout-timestamps > /dev/null 2>&1
